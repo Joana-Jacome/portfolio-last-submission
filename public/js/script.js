3e421fa712396469712ad5cc96c4df5828c9628b
Joana Jacome <jisjacome@gmail.com>
2018-05-30 09:02:57 +0200

    fixed jquery script as the slim version was not working

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,12 @@
-var $progressBar = $('.progress-bar');
+
+
+
 
 $(document).ready(function(){
+
+    var $progressBar = $('.progress-bar');
+    console.log($progressBar);
     $progressBar.animate({width: "70%"}, 100);
+    
 
 });

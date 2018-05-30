6430a76ea4f9c0c6c691866ab5ce09431dcf1b6c
Joana Jacome <jisjacome@gmail.com>
2018-05-31 00:03:23 +0200

    added changes to jquery script and added animationsto progress bars and text in hero image

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,39 @@
-
+$(document).ready(function() {
+    $('.my-name').hide().fadeIn(800);
+    $('.my-title').hide().delay(850).fadeIn(900);
+});
 
 
 
 $(document).ready(function(){
-
-    var $progressBar = $('.progress-bar');
-    console.log($progressBar);
-    $progressBar.animate({width: "70%"}, 100);
+      let $html = $('.html');
+      let $css = $('.css');
+      let $javascript = $('.javascript');
+      let $react = $('.react');
+      let $jQuery = $('.jQuery');
+      let $PHP = $('.PHP');
+      let $Laravel = $('.Laravel');
+    $html.delay(3500).animate({width: "65%"}, "linear");
+    $css.delay(3500).animate({width: "60%"}, "linear");
+    $javascript.delay(3500).animate({width: "55%"}, "linear");
+    $react.delay(3500).animate({width: "53%"}, "linear");
+    $jQuery.delay(3500).animate({width: "55%"},"linear");
+    $PHP.delay(3500).animate({width: "55%"},"linear");
+    $Laravel.delay(3500).animate({width: "55%"}, "linear");
     
 
 });
+
+
+        
+
+
+
+
+
+
+
+
+
+
+

529e0ee99338017adda6412ef01e379fc0d2b1e3
Joana Jacome <jisjacome@gmail.com>
2018-06-03 15:04:38 +0200

    added php sccript for form

--- public/index.php
+++ public/index.php
@@ -1,6 +1,61 @@
 <?php
 
+  // define variables and set to empty values
+$name_error = $email_error = "";
+$name = $email = $message = $success = "";
+
+//form is submitted with POST method
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $name_error = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $name_error = "Only letters and white space allowed"; 
+    }
+  }
+
+  if (empty($_POST["email"])) {
+    $email_error = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $email_error = "Invalid email format"; 
+    }
+  }
+
+
+  if (empty($_POST["message"])) {
+    $message = "";
+  } else {
+    $message = test_input($_POST["message"]);
+  }
   
+  if ($name_error == '' and $email_error == '' ){
+      $message_body = '';
+      unset($_POST['submit']);
+      foreach ($_POST as $key => $value){
+          $message_body .=  "$key: $value\n";
+      }
+      
+      $to = 'jisjacome@gmail';
+      $subject = 'Contact Form Submit';
+      if (mail($to, $subject, $message)){
+          $success = "Message sent, thank you for contacting me!";
+          $name = $email = $message = '';
+      }
+  }
+  
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 
 
 ?>
@@ -225,21 +280,24 @@
       </div>
 
       
-  <form>
+  <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
   <div class="form-group mt-5">
-      <input type="text" class="form-control" placeholder="Enter Name">
+      <input type="text" class="form-control" placeholder="Enter Name" name="name" value="<?= $name ?>">
+      <span class="error"><?= $name_error ?></span>
     </div>
     <div class="form-group">
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="email" name="email" value="<?= $email ?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <span class="error"><?= $email_error ?></span>
   </div>
   <div class="form-group">
-    <textarea class="form-control" id="exampleFormControlTextarea1"  placeholder="Enter Message" rows="3"></textarea>
+    <textarea class="form-control" id="exampleFormControlTextarea1"  placeholder="Enter Message" rows="3" value="<?= $message ?>" name="message"></textarea>
   </div>
   <div class="form-row text-center">
     <div class="col-12">
-        <button type="submit" class="btn btn-light font-weight-bold text-muted mb-5">Submit</button>
+        <button type="submit" name="submit" data-submit="...Sending" class="btn btn-light font-weight-bold text-muted mb-5">Submit</button>
     </div>
  </div>
+ <div class="success"><?= $success ?></div>
  </form>
 
       <div class="social">
